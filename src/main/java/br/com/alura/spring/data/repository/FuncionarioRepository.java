3a4bd97d2649a7160442f0a8b0c510f8f7266d90
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-09-21 00:27:00 -0300

    aula 3.2

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,7 +1,9 @@
 package br.com.alura.spring.data.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,8 @@ import br.com.alura.spring.data.orm.Funcionario;
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 	List<Funcionario> findByNome(String nome);
+	
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
+			+ "AND f.salario >= :salario AND f.dataContratacao = :data")
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
 }
