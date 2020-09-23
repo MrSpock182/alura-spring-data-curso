9f643801683cd4e22ad52797ff590e1513e841ee
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-09-23 00:16:20 -0300

    aula 5.1

--- src/main/java/br/com/alura/spring/data/orm/FuncionarioProjecao.java
+++ src/main/java/br/com/alura/spring/data/orm/FuncionarioProjecao.java
@@ -1,7 +1,5 @@
 package br.com.alura.spring.data.orm;
 
 public interface FuncionarioProjecao {
-	Integer get_Id();
-	String get_Nome();
-	Double get_Salario();
+
 }
--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
-import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
@@ -22,8 +21,6 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 			nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
 	
-	@Query(value = "SELECT f.id, f.nome, f.salario funcionarios f", 
-			nativeQuery = true)
-	List<FuncionarioProjecao> pesquisaFuncionarioSalario();
-	
+	@Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
+	void findFuncionarioSalario();
 }
