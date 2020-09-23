884c4a92183028d5ab2986efd5ec82d171eb83ad
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-09-22 23:54:04 -0300

    aula 5.1

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
@@ -23,6 +24,6 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 	
 	@Query(value = "SELECT f.id, f.nome, f.salario funcionarios f", 
 			nativeQuery = true)
-	void pesquisaFuncionarioSalario();
+	List<FuncionarioProjecao> pesquisaFuncionarioSalario();
 	
 }
