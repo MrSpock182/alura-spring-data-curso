2a759447f8c63c7e8e83bfd0790ee495b756be4a
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-09-27 23:26:49 -0300

    aula 6.2

--- src/main/java/br/com/alura/spring/data/specification/SpecificationFuncionario.java
+++ src/main/java/br/com/alura/spring/data/specification/SpecificationFuncionario.java
@@ -1,5 +1,7 @@
 package br.com.alura.spring.data.specification;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import br.com.alura.spring.data.orm.Funcionario;
@@ -9,6 +11,21 @@ public class SpecificationFuncionario {
 	public static Specification<Funcionario> nome(String nome) {
 		return (root, criteriaQuery, criteriaBuilder) -> 
 			criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+	}
+	
+	public static Specification<Funcionario> cpf(String cpf) {
+		return (root, criteriaQuery, criteriaBuilder) -> 
+			criteriaBuilder.equal(root.get("cpf"), cpf);
+	}
+	
+	public static Specification<Funcionario> salario(Double salario) {
+		return (root, criteriaQuery, criteriaBuilder) -> 
+			criteriaBuilder.greaterThan(root.get("salario"), salario);
+	}
+	
+	public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
+		return (root, criteriaQuery, criteriaBuilder) -> 
+			criteriaBuilder.greaterThan(root.get("dataContratacao"), dataContratacao);
 	}
 	
 }
