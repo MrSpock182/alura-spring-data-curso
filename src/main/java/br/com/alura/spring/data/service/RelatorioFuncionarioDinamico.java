a7fb7036e1d42be1561fb3939a7ddbb8ffed14ea
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-09-27 23:27:20 -0300

    aula 6.2

--- src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -1,18 +1,24 @@
 package br.com.alura.spring.data.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 import br.com.alura.spring.data.specification.SpecificationFuncionario;
 
+@Service
 public class RelatorioFuncionarioDinamico {
 
-private final FuncionarioRepository funcionarioRepository;
+	private final FuncionarioRepository funcionarioRepository;
 	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
 	public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
 		this.funcionarioRepository = funcionarioRepository;
 	}
@@ -21,8 +27,41 @@ private final FuncionarioRepository funcionarioRepository;
 		System.out.println("Digite o nome");
 		String nome = scanner.next();
 		
+		if(nome.equalsIgnoreCase("NULL")) {
+			nome = null;
+		}
+		
+		System.out.println("Digite o cpf");
+		String cpf = scanner.next();
+		
+		if(cpf.equalsIgnoreCase("NULL")) {
+			cpf = null;
+		}
+		
+		System.out.println("Digite o Salario");
+		Double salario = scanner.nextDouble();
+		
+		if(salario == 0) {
+			salario = null;
+		}
+		
+		System.out.println("Digite o data de contratacao");
+		String data = scanner.next();
+		
+		LocalDate dataContratacao;
+		if(data.equalsIgnoreCase("NULL")) {
+			dataContratacao = null;
+		} else {
+			dataContratacao = LocalDate.parse(data, formatter);
+		}
+		
 		List<Funcionario> funcionarios = funcionarioRepository.findAll(Specification
-				.where(SpecificationFuncionario.nome(nome)));
+				.where(
+						SpecificationFuncionario.nome(nome)
+						.and(SpecificationFuncionario.cpf(cpf))
+						.and(SpecificationFuncionario.salario(salario))
+						.and(SpecificationFuncionario.dataContratacao(dataContratacao))
+						));
 	}
 	
 }
