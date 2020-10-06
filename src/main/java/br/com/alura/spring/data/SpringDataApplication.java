2d4e686648fcda071d9a81166997d8549d9f5e3f
Kleber Nunes <klebernunes182@gmail.com>
2020-10-06 17:03:21 -0300

    aula 2.1

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.service.CrudCargoService;
 
 @SpringBootApplication
